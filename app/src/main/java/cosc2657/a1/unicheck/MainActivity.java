91d490c169fd219a0d2ba91d3f0289193931d92d
diablo <quangan186@gmail.com>
2022-11-11 17:56:43 +0700

    navbar

--- app/src/main/java/cosc2657/a1/unicheck/MainActivity.java
+++ app/src/main/java/cosc2657/a1/unicheck/MainActivity.java
@@ -1,26 +1,42 @@
 package cosc2657.a1.unicheck;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.core.view.WindowInsetsControllerCompat;
 
-import android.os.Build;
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.WindowManager;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        hideSystemBars();
+
+        navigationView = findViewById(R.id.bottomNav);
+        navigationView.setOnItemReselectedListener(new NavigationBarView.OnItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        Toast.makeText(MainActivity.this, "Home page", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.liked:
+                        Toast.makeText(MainActivity.this, "Liked", Toast.LENGTH_LONG).show();
+                        break;
+                    default:
+                }
+            }
+        });
     }
 
-    private void hideSystemBars() {
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT){
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        }
-    }
 }
